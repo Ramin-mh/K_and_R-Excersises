095e80145d58e614a2c947aac5ae0bfe6c430888
ramin <ramin.mh.007@gmail.com>
2026-04-23 10:34:13 +0530

    Initial Commit

---
+++ Code_Examples/undcl.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXTOKEN 100
+
+enum { NAME, PARENS, BRACKETS };
+
+int buf = EOF;
+int getch(void);
+void ungetch(int);
+
+int gettoken(void);
+int tokentype;
+
+char token[MAXTOKEN];
+char out[1000];
+
+/* undcl: convert word descriptions to declarations */
+int main(void) {
+    int type;
+    char temp[MAXTOKEN];
+
+    while (gettoken() != EOF) {
+        strcpy(out, token);
+        while ((type = gettoken()) != '\n') {
+            if (type == PARENS || type == BRACKETS)
+                strcat(out, token);
+            else if (type == '*') {
+                sprintf(temp, "(*%s)", out);
+                strcpy(out, temp);
+            } else if (type == NAME) {
+                sprintf(temp, "%s %s", token, out);
+                strcpy(out, temp);
+            } else
+                printf("invalid input at %s\n", token);
+        }
+        printf("%s\n", out);
+    }
+    return 0;
+}
+
+int getch(void){
+    int tmp;
+    
+    if (buf == EOF){
+        return getchar();   
+    }
+    else {
+        tmp = buf;
+        buf = EOF;
+        return tmp;    
+    }
+}
+
+void ungetch(int c){
+    if (buf == EOF){
+        buf = c;
+    }
+    else {
+        fprintf(stderr, "Buffer Overflow!, exiting...\n");
+        exit(1);
+    }
+}
+
+int gettoken(void) /* return next token */
+{
+    int c;
+    char *p = token;
+
+    while ((c = getch()) == ' ' || c == '\t') { };
+    if (c == '(') {
+        if ((c = getch()) == ')') {
+            strcpy(token, "()");
+            return tokentype = PARENS;
+        }
+        else {
+            ungetch(c);
+            return tokentype = '(';
+        }
+    }
+    else if (c == '[')
+    {
+        for (*p++ = c; (*p++ = getch()) != ']'; );
+        *p = '\0';
+        return tokentype = BRACKETS;
+    }
+    else if (isalpha(c))
+    {
+        for (*p++ = c; isalnum(c = getch()); )
+            *p++ = c;
+        *p = '\0';
+        ungetch(c);
+        return tokentype = NAME;
+    }
+    else
+        return tokentype = c;
+}
