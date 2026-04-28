25a8077198541d6bce1440dbd44258b457bc06ac
ramin <ramin.mh.007@gmail.com>
2026-04-28 12:21:41 +0530

    version 1.0

--- 6-3_word_xref.c
+++ 6-3_word_xref.c
@@ -4,16 +4,36 @@
 #include <string.h>
 
 #define MAXWORD 100
+#define MAXLEN 100
+
+struct treeNode {
+    char *word;
+    int line_nos[MAXLEN];
+    int top;
+
+    struct treeNode *left;
+    struct treeNode *right;
+};
+
+struct treeNode *addTree(struct treeNode *p, char *w, int line_no);
+void printTree(struct treeNode *p);
 
 int getword(char *w, int size, int *line_no);
 
 int main(void) {
+    struct treeNode *root = NULL;
+    
     char w[MAXWORD];
     int k;
     int line_no = 1;
 
+    if (MAXLEN <= 0) {
+        fprintf(stderr, "No size allocated to array! exiting..\n");
+        return 1;
+    }
+
     while ((k = getword(w, MAXWORD, &line_no)) != EOF && k != '\0') {
-        printf("%d: %s\n", line_no, w);
+        root = addTree(root, w, line_no);
     }
 
     if (k == '\0') {
@@ -21,7 +41,61 @@ int main(void) {
         return 1;
     }
 
+    printTree(root);
+
     return 0;
+}
+
+struct treeNode *allocTree(void) {
+    return malloc(sizeof(struct treeNode));
+}
+
+struct treeNode *addTree(struct treeNode *p, char *w, int line_no) {
+    int k;
+    
+    if (p == NULL) {
+        if ((p = allocTree()) == NULL) {
+            fprintf(stderr, "allocation failed\n");
+            exit(1);
+        }
+        p->word = strdup(w);
+        p->top = 0;
+        p->line_nos[p->top++] = line_no;
+
+        p->left = p->right = NULL;
+    }
+    else if ((k = strcmp(w, p->word)) == 0) {
+        if (p->top >= MAXLEN) {
+            fprintf(stderr, "Array size exceeded!, exiting..\n");
+            exit(1);
+        }
+
+        if (p->line_nos[p->top - 1] != line_no) {
+            p->line_nos[p->top++] = line_no;
+        }
+    }
+    else if (k < 0) {
+        p->left = addTree(p->left, w, line_no);
+    }
+    else {
+        p->right = addTree(p->right, w, line_no);
+    }
+
+    return p;
+}
+
+void printTree(struct treeNode *p) {
+    int i;
+    
+    if (p != NULL) {
+        printTree(p->left);
+        printf("%s:-\n", p->word);
+        for (i = 0; i < p->top; i++) {
+            printf("%d, ", p->line_nos[i]);
+        }
+        putchar('\n');
+        printTree(p->right);
+    }
 }
 
 int is_noise_word(char *s) {
